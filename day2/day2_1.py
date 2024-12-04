28aae863c5a5d4a5b3d410a9388244377a644131
tomas1978 <tomas.nilsson78@gmail.com>
2024-12-04 23:29:06 +0100

    Continued on Day2 problem
    Not finished

--- day2/day2_1.py
+++ day2/day2_1.py
@@ -1,8 +1,30 @@
 #day 2 problem 2, just started!
 file = open("inputSmall.txt", "r")
 
-while True:
+content=1
+
+def isIncreasing(list):
+    for i in range(0,len(list)-1):
+        if(list[i+1]<list[i]):
+            return False
+    return True
+
+def isDecreasing(list):
+    for i in range(0,len(list)-1):
+        if(list[i+1]>list[i]):
+            return False
+    return True
+
+while content:
     content=file.readline()
-    print(content)
-    if(not(content)):
-        break
+    numbers=content.split()
+    if content:
+        for i in range(0,len(numbers)):
+            numbers[i]=int(numbers[i])    
+        if isDecreasing(numbers):
+            print("Decreasing")
+        if isIncreasing(numbers):
+            print("Decreasing")
+
+        
+
