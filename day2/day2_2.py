0ec1ddb4297670bc7673a993db4a0cdf78798446
tomas1978 <tomas.nilsson78@gmail.com>
2024-12-06 21:49:16 +0100

    Day 2_2 continued
    Not solved, gives a too high value

--- day2/day2_2.py
+++ day2/day2_2.py
@@ -1,25 +1,35 @@
-#day 2 problem 1, solved!
+#day 2 problem 2, gives a too high result!
 file = open("input.txt", "r")
 
 content=1
 numberOfSafeReports=0
 
+
 def isIncreasing(list):
+    removed=0
     for i in range(0,len(list)-1):
         if(list[i+1]<=list[i]):
-            return False
+            removed+=1
+            if removed>1:
+                return False
     return True
 
 def isDecreasing(list):
+    removed=0
     for i in range(0,len(list)-1):
         if(list[i+1]>=list[i]):
-            return False
+            removed+=1
+            if removed>1:            
+                return False
     return True
 
 def isCorrectDistance(list):
+    removed=0
     for i in range(0,len(list)-1):
         if(abs(list[i+1]-list[i])<1 or abs(list[i+1]-list[i])>3 ):
-            return False
+            removed+=1
+            if removed>1:            
+                return False
     return True
 
 while content:
