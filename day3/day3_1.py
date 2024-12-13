723e1324872732a43dc4297641bdd9d959041a25
tomas1978 <tomas.nilsson78@gmail.com>
2024-12-13 17:16:17 +0100

    Some parsing
    Trying to find mul(,) in string and their positions

--- day3/day3_1.py
+++ day3/day3_1.py
@@ -1,4 +1,20 @@
-#day 3 problem 1, just started
+#day 3 problem 1, 11 dec just started
+#12 dec creating a list with positions of all mul instructions
 file = open("inputSmall.txt", "r")
 data=file.readline()
-print(data.find("mul"))
+mulPositions=[]
+lastMul=0
+while lastMul!=-1:
+    print(lastMul)
+    lastMul=data.find("mul", lastMul+1, len(data))
+    if lastMul!=-1:
+        mulPositions.append(lastMul)
+print(mulPositions)
+
+for i in range(0,len(mulPositions)):
+    leftParanthesis=data.find("(",mulPositions[i])
+    comma=data.find(",")
+    rightParanthesis=data.find(")",mulPositions[i])
+    print("left: ",leftParanthesis, "comma", comma, "right",rightParanthesis)
+
+    
